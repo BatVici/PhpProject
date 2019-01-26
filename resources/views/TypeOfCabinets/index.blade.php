c33f827a6557cd791c80bc76393114adee581350
BatVici <BatVici@users.noreply.github.com>
2019-01-26 23:52:45 +0200

    Updating project

--- resources/views/TypeOfCabinets/index.blade.php
+++ resources/views/TypeOfCabinets/index.blade.php
@@ -5,15 +5,15 @@
 
     </tr>
     <form>
-        @foreach($typeofcabinets as $typeofcabinet)
+        @foreach($types as $type)
             <tr>
-                <td>{{$typeofcabinet-> Type}}</td>
-                <td>{{$typeofcabinet -> Description}}</td>
+                <td>{{$type-> Type}}</td>
+                <td>{{$type -> Description}}</td>
 
                 <td>
 
-                    <a class="btn btn-primary"  href="{{route('$typeofcabinets.show', $typeofcabinet  -> id)}}" method="POST"> SHOW</a>
-                    <a class="btn btn-primary"  href="{{route('$typeofcabinets.edit', $typeofcabinet  -> id)}}" method="POST"> EDIT</a>
+                    <a class="btn btn-primary"  href="{{route('types.show', $type  -> id)}}" method="POST"> SHOW</a>
+                    <a class="btn btn-primary"  href="{{route('types.edit', $type  -> id)}}" method="POST"> EDIT</a>
                 </td>
             </tr>
         @endforeach
---
+++ resources/views/TypeOfCabinets/show.blade.php
@@ -0,0 +1,11 @@
+<table>
+    <tr>
+        <th>Type</th>
+        <th>Description</th>
+    </tr>
+    <tr>
+        <td>{{$types -> Type}}</td>
+        <td>{{$types -> Description}}</td>
+
+    </tr>
+</table>
