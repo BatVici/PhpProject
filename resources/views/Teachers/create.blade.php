59fd198f2e1bfdb9e271e97bfa4069c0ad2542d8
BatVici <BatVici@users.noreply.github.com>
2019-01-27 19:03:50 +0200

    Adding layout

--- resources/views/Teachers/create.blade.php
+++ resources/views/Teachers/create.blade.php
@@ -1,3 +1,27 @@
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="card uper">
+        <div class="card-header">
+            Add Teacher
+        </div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div><br />
+            @endif
+
+
 <form action="/teachers" method="post">
     {{csrf_field()}}
     <div class="form-group">
@@ -14,3 +38,6 @@
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 </form>
+        </div>
+    </div>
+@endsection
--- resources/views/Teachers/edit.blade.php
+++ resources/views/Teachers/edit.blade.php
@@ -1,3 +1,26 @@
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="card uper">
+        <div class="card-header">
+            Edit Teacher
+        </div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div><br />
+            @endif
+
 <form action="{{action('TeachersController@update',$teacher->id)}}" method="post">
     {{csrf_field()}}
     <input type="hidden" class="form-control" name="_method" value="patch"/>
@@ -15,3 +38,6 @@
         <button type="submit" class="btn btn-primary">Save</button>
     </div>
 </form>
+        </div>
+    </div>
+@endsection
