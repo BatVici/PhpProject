21e0a31f35af16b81dd3db1f0a20c92f3ed60bc1
BatVici <BatVici@users.noreply.github.com>
2019-01-27 19:04:13 +0200

    Updating blades

--- resources/views/Cabinets/index.blade.php
+++ resources/views/Cabinets/index.blade.php
@@ -1,24 +1,48 @@
-<table>
-    <tr>
-        <th>Number</th>
-        <th>Capacity</th>
-        <th>Type</th>
-        <th>Available</th>
-        <th>Buttons</th>
-    </tr>
-    <form>
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="uper">
+        @if(session()->get('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div><br />
+        @endif
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <td>Number</td>
+                <td>Capacity</td>
+                <td>Type</td>
+                <td>Available</td>
+                <td colspan="2">Action</td>
+            </tr>
+            </thead>
+            <tbody>
 @foreach($cabinets as $cabinet)
  <tr>
      <td>{{$cabinet -> Number}}</td>
      <td>{{$cabinet -> Capacity}}</td>
      <td>{{$cabinet -> Type}}</td>
      <td>{{$cabinet -> Available}}</td>
-     <td>
 
-         <a class="btn btn-primary"  href="{{route('cabinets.show', $cabinet  -> id)}}" method="POST"> SHOW</a>
-         <a class="btn btn-primary"  href="{{route('cabinets.edit', $cabinet  -> id)}}" method="POST"> EDIT</a>
+     <td>
+         <form action="{{ route('cabinets.destroy', $cabinet->id)}}" method="post">
+             @csrf
+             @method('DELETE')
+             <button class="btn btn-danger" type="submit">Delete</button>
+         </form>
      </td>
+     <td><a href="{{ route('cabinets.edit',$cabinet->id)}}" class="btn btn-primary" type="submit" method="POST">Edit</a></td>
+     <td><a href="{{route('cabinets.show', $cabinet  -> id)}}" class="btn btn-primary"  method="POST"> Show</a></td>
  </tr>
 @endforeach
-    </form>
-</table>
+            </tbody>
+    </table>
+    </div>
+@endsection
--- resources/views/Cabinets/show.blade.php
+++ resources/views/Cabinets/show.blade.php
@@ -1,10 +1,29 @@
-<table>
-    <tr>
-        <th>Number</th>
-        <th>Capacity</th>
-        <th>Type</th>
-        <th>Available</th>
-    </tr>
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="uper">
+        @if(session()->get('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div><br />
+        @endif
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <td>Number</td>
+                <td>Capacity</td>
+                <td>Type</td>
+                <td>Available</td>
+            </tr>
+            </thead>
+            <tbody>
+
         <tr>
             <td>{{$cabinet -> Number}}</td>
             <td>{{$cabinet -> Capacity}}</td>
@@ -12,3 +31,6 @@
             <td>{{$cabinet -> Available}}</td>
         </tr>
 </table>
+
+    </div>
+@endsection
